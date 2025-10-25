29411de4b5685a4cc23831fd54c71a339cecc160
Boris <boris@xx1.nl>
2025-10-25 10:29:10 +0200

    first commit

---
+++ src/main/java/nl/xx1/padel/api/PadelBotApiApplication.java
@@ -0,0 +1,13 @@
+package nl.xx1.padel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadelBotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadelBotApiApplication.class, args);
+	}
+
+}
